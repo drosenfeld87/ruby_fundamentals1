2f265547f112420a23b39ba82248b5250a75cc2b
debbie <drosenfeld87@gmail.com>
2018-01-29 15:36:45 -0500

    complete exercise3

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,11 @@
-puts "what is your name?"
-user_name = gets
-puts "Hello, #{user_name}"
+# puts "what is your name?"
+# user_name = gets
+# puts "Hello, #{user_name}"
+
+# puts "what is your name?"
+# user_name = gets.chomp
+# puts "Hello, #{user_name}"
+
+puts "what year were you born"
+user_birthyear = gets
+puts "#{user_birthyear}"
