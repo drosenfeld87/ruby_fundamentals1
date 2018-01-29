17283b51c7356df66ef4136b938a435e78fbdd45
debbie <drosenfeld87@gmail.com>
2018-01-29 15:00:18 -0500

    complete exercise2

---
+++ exercise2.rb
@@ -0,0 +1,16 @@
+# question 1
+# calculate tip for $55 meal
+# meal-cost = 55.00
+# tip-percent = 15
+
+# puts (55 * 0.15)
+
+# question 2
+# add string and integer
+# puts "Tip amount is " + (55 * 0.15) .to_s
+
+# question 3
+# puts "Numbers 45628 multiplied by 7839 equals #{45628 * 7839}" .to_s
+
+#question 4
+# puts (10 < 20 && 30 < 20) || !(10 == 11)
