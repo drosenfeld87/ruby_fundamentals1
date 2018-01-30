0a94463259616130cdffc182847de047315a75b4
debbie <drosenfeld87@gmail.com>
2018-01-30 12:38:02 -0500

    complete exercise5

--- exercise5.rb
+++ exercise5.rb
@@ -1,12 +1,15 @@
 distance_from_home = 0
-puts "would you like to walk or run?"
+count = 0
+
+while count < 5
+  puts "would you like to walk or run?"
   answer = gets.chomp
-if
-  answer == "walk"
-  distance_from_home = distance_from_home + 1
-  puts "distance from home is " + distance_from_home + "km"
-elsif
-  answer == "run"
-  distance_from_home = distance_from_home + 5
-  puts "distance from home is "
+  if answer == "walk"
+    distance_from_home = distance_from_home + 1
+    puts "distance from home is #{distance_from_home}"
+  elsif answer == "run"
+    distance_from_home = distance_from_home + 5
+    puts "distance from home is #{distance_from_home}"
+  end
+  count = count + 1
 end
